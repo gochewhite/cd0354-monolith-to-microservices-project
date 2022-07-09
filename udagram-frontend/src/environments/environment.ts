4bb4f894302666b8b5572c1e5c93b411b5c62686
Goodness Obah <49002276+gochewhite@users.noreply.github.com>
2022-07-09 20:51:07 +0100

    works

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae64695f4d0714218a6083fc5cc16cc8-1507617373.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
